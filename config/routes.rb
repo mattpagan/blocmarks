215a41a3254f11c7473b810eb7764f14b7c8c2ac
Matt Pagan <matthew.pagan@me.com>
2015-09-21 05:00:40 -0400

    Receive Incoming Emails, progress, still need to test the code

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users
+  
   get 'welcome/index'
 
   get 'welcome/about'
+
+  post :incoming, to: 'incoming#create'
 
   root to: 'welcome#index'
 end
